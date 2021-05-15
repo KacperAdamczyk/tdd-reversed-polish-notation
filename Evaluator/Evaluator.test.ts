02114bd2be7d0a077520807a07fb73df9c25e19f
Kacper Adamczyk <kacper.k.adamczyk@gmail.com>
2021-05-15 21:53:15 +0200

    Parenthesis - test

--- Evaluator/Evaluator.test.ts
+++ Evaluator/Evaluator.test.ts
@@ -13,4 +13,8 @@ describe('Evaluator', () => {
   it('should evaluate expression with priorities', () => {
     expect(evaluator.evaluate('1 - 2 * 3 / 2 ^ 2')).toBe(-0.5);
   });
+
+  it('should evaluate expression with parenthesis', () => {
+    expect(evaluator.evaluate('(1 - 2) * 3')).toBe(-3);
+  });
 });
